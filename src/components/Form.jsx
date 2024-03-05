602e9ee074f33dadc91b6a7d7dafa702694126d7
Vishal Shah <vishalshahshh@gmail.com>
2024-03-05 18:58:45 +0530

    evening commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,45 +3,82 @@ import { useFormik } from "formik";
 import React, { useEffect, useState } from "react";
 import Select from "react-select";
 
-
-
-
-const options = books.map(({name}) =>{
-  return name
-})
-
 function Form() {
+  const [books, setBooks] = useState(); //DATABASE
+  const [count,setCount] = useState(0);//Count for PlusButton
 
-    const[books,setBooks] = useState();
+  //FETCH DATA
+  useEffect(() => {
+    axios.get("/database/Records.json").then((Response) => {
+      setBooks(Response.data);
+    });
+  }, []);
 
-    //FETCH DATA
-    useEffect(() =>{
-        axios.get("/database/Records.json")
-        .then((Response)=>{
-            setBooks(Response.data)
-        })
-    },[])
+  //OPTIONS FOR BOOK NAME
+  const optionsBookName = books?.map(({name}) => {
+    return {value:name,label:name}
+  })
+ //OPTIONS FOR BOOK Chapter
+  const [ optionsBookChapter, setOptionsBookChapter] = useState();
+ 
+
+
+  //CHANGE METHOD OF CHAPTERS
+  const myhandleChangeChapterName = (e) => {
+  values.bookChapter = e.map(values => values.value)
+  console.log(values)
+}
+
+  //PLUS BUTTON CLICKED
+  const handlePlusClicked = () =>{
     
+  }
 
+  //FORMIK
   const initialValues = {
-    id: crypto.randomUUID(),
-    token: crypto.randomUUID(),
-    name: "",
-    contactNumber: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
+    titleOfPlan:'',
+    bookName:'',
+    bookChapter:''
   };
 
   const { values, handleSubmit, handleChange, handleReset, errors, touched } =
     useFormik({
       initialValues,
+
+   
       onSubmit: (value, action) => {},
     });
 
-  return (
-    <div className="w-[80%] mx-auto mt-12">
-        {console.log(books)}
+    //CHANGE METHOD OF BOOKNAME
+    const myhandleChangeBookName = (e) => {
+
+          var test = books.filter((book) =>{
+              if(book.name === e.value)
+              {
+                return book
+              }
+            })  
+            
+          values.bookName =  e.value ; // set value of BookName(Formik)
+          let arrayofChapters=(test[0].chapters).map(bookChapterName => bookChapterName.name)//options of Chapters 
+            
+          /* refrence logic
+          console.log(test,"test")
+          console.log( " bname: ",values.bookName," CName:",values.bookChapter," titleofPlan:",values.titleOfPlan) */
+          
+
+          //SETING OPTIONS FOR CHAPTERS
+          setOptionsBookChapter( 
+              arrayofChapters?.map((chapters) => { 
+              return { value: chapters , label: chapters } 
+            })
+          )
+    }
+
+      return (
+        <div className="w-[80%] mx-auto mt-12">
+
+
       <section className=" w-full flex items-center justify-center">
         <div className="w-full bg-white rounded-lg shadow dark:border   xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className=" w-full p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -60,94 +97,36 @@ function Form() {
                   type="text"
                   name="titleOfPlan"
                   onChange={handleChange}
-                  // value={values.name}
+                  value={values.titleOfPlan}
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Enter Unique Title of Plan"
                 />
               </div>
-              <div className="w-40">
+              <div className="w-full flex gap-2 justify-">
                 <Select
-                  name="BookName"
-                  //   onChange={(e) => handleChange1(e, id)}
-                  options={options}
-                  styles={{ width: "50px" }}
+                  name="bookName"
+                  onChange={(e) => myhandleChangeBookName(e)}
+                  options={optionsBookName}
+                  value={optionsBookName?.value}
                 />
-
-                
+                <Select
+                  name="chapters"
+                  onChange={(e) => myhandleChangeChapterName(e)}
+                  options={optionsBookChapter}
+                  value={optionsBookChapter?.value}
+                  isMulti
+                />
               </div>
               <div>
                 <button
-                  type="submit"
+                  type="button"
+                  onClick={handlePlusClicked}
                   className="w-[10] text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                    +
+                  +
                 </button>
               </div>
-              {/* <div>
-                <label
-                  htmlFor="contactNumber"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Contact Number
-                </label>
-                <input
-                  type="Number"
-                  // name="contactNumber"
-                  onChange={handleChange}
-                  // value={values.contactNumber}
-                  id="username"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="(+1) 0123456789"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  E-mail
-                </label>
-                <input
-                  type="email"
-                  // name="email"
-                  onChange={handleChange}
-                  // value={values.email}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="vishalshah@gmail.com"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  // name="password"
-                  onChange={handleChange}
-                  // value={values.password}
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                />
-              </div>
-              <div>
-                <label
-                  htmlFor="confirmPassword"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Confirm Password
-                </label>
-                <input
-                  type="password"
-                  // name="confirmPassword"
-                  onChange={handleChange}
-                  // value={values.confirmPassword}
-                  placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                />
-              </div> */}
+            
               <button
                 type="submit"
                 className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
